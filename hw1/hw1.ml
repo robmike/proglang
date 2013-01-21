a2becd8c0d630dfe55289d2f692afb7218181150
invalid <invalid@invalid>
2013-01-21 16:38:22 -0500

    hw1: q4

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -21,3 +21,6 @@ fun number_in_month(dates : date list, month : int) =
 
 fun number_in_months(dates : date list, months : int list) =
     foldl (fn(m : int, count : int) => if(number_in_month(dates, m) > 0) then count+1 else count) 0 months
+
+fun date_in_month(dates : date list, m : int) =
+    foldl (fn(d : date, out : date list) => if (m = (#2 d)) then d::out else out) [] dates
