62d1605c3f03f05e2ca8a3233301ff3c56f41b15
invalid <invalid@invalid>
2013-01-21 16:26:48 -0500

    hw1: q1, q2, q3

---
+++ hw1/hw1.ml
@@ -0,0 +1,23 @@
+val is_older = fn : (int * int * int) * (int * int * int) -> bool;
+val number_in_month = fn : (int * int * int) list * int -> int;
+val number_in_months = fn : (int * int * int) list * int list -> int;
+val dates_in_month = fn : (int * int * int) list * int -> (int * int * int) list;
+val dates_in_months = fn : (int * int * int) list * int list -> (int * int * int) list;
+val get_nth = fn : string list * int -> string;
+val date_to_string = fn : int * int * int -> string;
+val number_before_reaching_sum = fn : int * int list -> int;
+val what_month = fn : int -> int;
+val month_range = fn : int * int -> int list;
+val oldest = fn : (int * int * int) list -> (int * int * int) option;
+
+type date = (int * int * int);
+
+fun is_older(d1 : date, d2 : date) =
+    (* This is ugly *)
+    ((#1 d1) < (#1 d2)) andalso ((#2 d1) < (#2 d2)) andalso ((#3 d1) < (#3 d2))
+
+fun number_in_month(dates : date list, month : int) =
+    foldl (fn(y : date, x : int) => if (month = (#2 y)) then x + 1 else x) 0 dates
+
+fun number_in_months(dates : date list, months : int list) =
+    foldl (fn(m : int, count : int) => if(number_in_month(dates, m) > 0) then count+1 else count) 0 months
