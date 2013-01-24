b0e3d94eac247999e5105f2f5a807186fed004f9
invalid <invalid@invalid>
2013-01-24 14:22:27 -0500

    hw1: comment out type signatures

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -1,14 +1,14 @@
-val is_older = fn : (int * int * int) * (int * int * int) -> bool;
-val number_in_month = fn : (int * int * int) list * int -> int;
-val number_in_months = fn : (int * int * int) list * int list -> int;
-val dates_in_month = fn : (int * int * int) list * int -> (int * int * int) list;
-val dates_in_months = fn : (int * int * int) list * int list -> (int * int * int) list;
-val get_nth = fn : string list * int -> string;
-val date_to_string = fn : int * int * int -> string;
-val number_before_reaching_sum = fn : int * int list -> int;
-val what_month = fn : int -> int;
-val month_range = fn : int * int -> int list;
-val oldest = fn : (int * int * int) list -> (int * int * int) option;
+(* val is_older = fn : (int * int * int) * (int * int * int) -> bool; *)
+(* val number_in_month = fn : (int * int * int) list * int -> int; *)
+(* val number_in_months = fn : (int * int * int) list * int list -> int; *)
+(* val dates_in_month = fn : (int * int * int) list * int -> (int * int * int) list; *)
+(* val dates_in_months = fn : (int * int * int) list * int list -> (int * int * int) list; *)
+(* val get_nth = fn : string list * int -> string; *)
+(* val date_to_string = fn : int * int * int -> string; *)
+(* val number_before_reaching_sum = fn : int * int list -> int; *)
+(* val what_month = fn : int -> int; *)
+(* val month_range = fn : int * int -> int list; *)
+(* val oldest = fn : (int * int * int) list -> (int * int * int) option; *)
 
 type date = (int * int * int);
 
