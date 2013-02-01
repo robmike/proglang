dc7a017c3016f601058a0840184e691abc6fa8e0
invalid <invalid@invalid>
2013-01-31 22:21:44 -0500

    hw3: test file

---
+++ hw3/hw3test.ml
@@ -0,0 +1,28 @@
+val only_capitals = fn : string list -> string list
+val it = () : unit
+- only_capitals(["blah", "foo", "Okay", "Then"])
+= ;
+val it = ["Okay","Then"] : string list
+- [opening /tmp/sml3047RHP]
+val longest_string = fn : string list -> string
+val it = () : unit
+- longest_string(["blah", "foobarqux", "Okay", "Then"])
+= ;
+val it = "foobarqux" : string
+- longest_string(["blah", "foo", "Okay", "Then"])
+= ;
+val it = "blah" : string
+- 
+first_answer (fn x => x) [NONE, NONE, SOME 4];
+val it = 4 : int
+- first_answer (fn x => x) [NONE, NONE, SOME 4];
+
+- all_answers (fn (x) => x) [[NONE, SOME 3]];
+val it = SOME [3] : int list option
+- all_answers (fn (x) => x) [[NONE, NONE, SOME 4], [NONE, SOME 3]];
+val it = SOME [4,3] : int list option
+- all_answers (fn (x) => x) [];
+stdIn:17.1-17.29 Warning: type vars not generalized because of
+   value restriction are instantiated to dummy types (X1,X2,...)
+val it = SOME [] : ?.X1 list option
+
