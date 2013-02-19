c9834e315a616e4506aa763ba061d9eb6dba76e0
invalid <invalid@invalid>
2013-02-18 21:14:45 -0500

    hw5: q3

--- hw5/hw5.rkt
+++ hw5/hw5.rkt
@@ -9,3 +9,10 @@
 (define string-append-map
   (lambda (xs suffix)
     (map (lambda (x) (string-append x suffix)) xs)))
+
+(define list-n-mod
+  (lambda (xs n)
+    (cond [(< n 0) (error "list-nth-mod: negative number")]
+          [(= 0 (length xs)) (error "list-nth-mod: empty list")]
+          [(car (list-tail xs (remainder n (length xs))))])))
+
