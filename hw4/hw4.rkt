ff70c93c2aaec2bb9f4e80c1f89481aaabc23aab
invalid <invalid@invalid>
2013-02-22 15:50:15 -0500

    remove unused streams

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -71,7 +71,3 @@
                 (set! idx (modulo (+ 1 idx) n))
               res])))
 
-(define ones (lambda ()
-               (cons 1 ones)))
-
-(define nats (letrec ([f (lambda (x) (cons x (lambda () (f (+ x 1)))))]) (lambda () (f 1))))
