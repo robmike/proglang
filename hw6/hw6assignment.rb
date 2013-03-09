b151c8ca88f86ac7ff8a80a96260bda804564d61
invalid <invalid@invalid>
2013-03-09 14:19:17 -0500

    hw6: Looks okay, not submitted

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -27,8 +27,19 @@ class MyPiece < Piece
   All_My_Pieces = All_Pieces  +
     [[[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
       [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
-     rotations([[0, 0], [0, 1], [1, 0]]), # small l
+     rotations([[0, 0], [0, -1], [-1, 0]]), # small l
      rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1, -1]])]
+
+  # All_My_Pieces =
+  #   [[[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+  #     [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+  #    rotations([[0, 0], [0, -1], [-1, 0]]), # small l
+  #    rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1, -1]])]
+
+  # All_My_Pieces = 
+  #   [rotations([[0, 0], [0, -1], [-1, 0]]), # small l
+  #    ]
+
 
   def self.set_piece(point_array, board)
     MyPiece.new(point_array, board)
@@ -42,10 +53,28 @@ end
 
 class MyBoard < Board
   # your enhancements here
-  Cheat_Cost = 10
+  Cheat_Cost = 100
+  Cheat_Piece = [[[0,0]]]
+
+  def initialize (game)
+    super(game)
+    @current_block = MyPiece.next_piece(self)
+  end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..locations.size-1).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 
   def cheat
-    if @score >= Cheat_Cost and @current_block != Cheat_Piece
+    if @score >= Cheat_Cost and !Cheat_Piece.include?(@current_block.current_rotation)
       puts "cheating"
       @score -= Cheat_Cost
       @cheat_next_piece = true
@@ -56,7 +85,7 @@ class MyBoard < Board
   def next_piece
     puts "my next_piece"
     if @cheat_next_piece
-      @current_block = MyPiece.new([[[0,0]]], self)
+      @current_block = MyPiece.new(Cheat_Piece, self)
       @cheat_next_piece = false
     else
       @current_block = MyPiece.next_piece(self) 
